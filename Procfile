0650f25eb400e267613d34c2f8b6bc8280e327b2
Ahmed Fangary <ahmedfangary92@yahoo.com>
2021-07-28 07:04:45 +0200

    make it better

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn todopro.wsgi --log-file -
+web: gunicorn todoproject.wsgi --log-file -
--- todoproject/settings.py
+++ todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g4rgo+^-jj77g8*a*a@n9sjn3-qavmyj(d2)l7kq5alc&!mi7%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['todopro2.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
